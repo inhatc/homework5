f25b0e6fcac9b32f18112347dfd71cf9ba79ea28
201244048 <vochelle@naver.com>
2016-10-20 20:43:35 +0900

    gugudan2 수정완료

--- src/Homework5/GuGuDan2.java
+++ src/Homework5/GuGuDan2.java
@@ -13,18 +13,21 @@ public class GuGuDan2 {
             System.out.print("짝수단 : E 홀수단 : O (종료는 : Q)");
             strData = scData.next().trim().toLowerCase();
             if("e".equals(strData)){
-                for(int i=2; i<=8; i=i+2){
                     for(int j=1; j<=9; j++){
-                        System.out.println(i+"*"+j+"="+(i*j));
+                        System.out.print(2+"*"+j+"="+2*j+"\t"+
+                                        4+"*"+j+"="+4*j+"\t"+
+                                        6+"*"+j+"="+6*j+"\t"+
+                                        8+"*"+j+"="+8*j+"\t"+"\n");
                     }
-                }
             }
             else if("o".equals(strData)){
-                for(int i=1; i<=9; i=i+2){
                     for(int j=1; j<=9; j++){
-                        System.out.println(i+"*"+j+"="+(i*j));
+                        System.out.print(1+"*"+j+"="+1*j+"\t"+
+                                3+"*"+j+"="+3*j+"\t"+
+                                5+"*"+j+"="+5*j+"\t"+
+                                7+"*"+j+"="+7*j+"\t"+
+                                9+"*"+j+"="+9*j+"\n");
                     }
-                }
             }
             else if("q".equals(strData)){ new ExamForWhile().start(); }
             else { System.out.println("입력값 확인하세요."); }
